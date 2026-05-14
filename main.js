f090646e87b3067049b40a859239c47cc8902953
vendavid05 <ven.david05@gmail.com>
2026-05-14 11:31:22 +0200

    Audiomanageres cuccok

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 import DialogSystem from './modules/DialogSystem.js';
 import DialogPanel from './ui/DialogPanel.js';
+import { AudioManager } from './modules/AudioManager.js';   
 
 const mockCharacterData = {
     modifyTraits(modifier) {
         console.log('Traits modified:', modifier);
     }
 };
-
+const audioManager = new AudioManager()
 const dialogSystem = new DialogSystem(mockCharacterData);
 const container = document.getElementById('dialog-container');
 const dialogPanel = new DialogPanel(dialogSystem, container);
